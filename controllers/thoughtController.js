78cc681383ebf0b54e2e387fdd69c6e391e1202b
Kenji Fleming <kwf1999@gmail.com>
2023-03-16 19:30:18 -0400

    worked on routes and their respective controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,1 +1,60 @@
 //thoughtController.js
+const { Thought, User } = require('../models');
+
+module.exports = {
+    // get all thoughts
+    getAllThoughts(req, res) {
+        Thought.find({})
+        .then(ThoughtData => res.json(ThoughtData))
+        .catch((err) => res.status(400).json(err));
+    },
+    // get one thought by id
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.id })
+        .then ((ThoughtData) =>  res.JSON(ThoughtData))
+        .catch((err) => res.status(400).json(err));
+    },
+    // createThought
+    createThought({ body }, res) {
+        Thought.create(body)
+        .then((ThoughtData) => res.json(ThoughtData))
+        .catch((err) => res.status(400).json(err));
+    },
+    // update thought by id
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.id },
+            { $set: body },
+            { runValidators: true, new: true }
+        )
+        .then((ThoughtData) => res.json(ThoughtData))
+        .catch((err) => res.status(400).json(err));
+    },
+    // delete thought
+    deleteThought({ params }, res) {
+        Thought.findOneAndDelete({ _id: params.id })
+        .then((ThoughtData) => res.json(ThoughtData))
+        .catch((err) => res.status(400).json(err));
+    },
+    // add reaction
+    addReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $addToSet: { reactions: body } },
+            { runValidators: true, new: true }
+        )
+        .then((ThoughtData) => res.json(ThoughtData))
+        .catch((err) => res.status(400).json(err));
+    },
+    // delete reaction
+    deleteReaction({ params }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $pull: { reactions: { reactionId: params.reactionId } } },
+            { runValidators: true, new: true }
+        )
+        .then((ThoughtData) => res.json(ThoughtData))
+        .catch((err) => res.status(400).json(err));
+    }
+};
+
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,60 @@
 //userController.js
+const { User, Thought } = require('../models');
+
+module.exports = {
+    // get all users
+    getAllUsers(req, res) {
+        User.find({})
+        .then(UserData => res.json(UserData))
+        .catch((err) => res.status(400).json(err));
+    },
+    // get one user by id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .then ((UserData) =>  res.JSON(UserData))
+        .catch((err) => res.status(400).json(err));
+    },
+    // createUser
+    createUser({ body }, res) {
+        User.create(body)
+        .then((UserData) => res.json(UserData))
+        .catch((err) => res.status(400).json(err));
+    },
+    // update user by id
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate( 
+            { _id: params.id },
+            { $set: body },
+            { runValidators: true, new: true }
+        )
+        .then((UserData) => res.json(UserData))
+        .catch((err) => res.status(400).json(err));
+    },
+    // delete user
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+        .then((UserData) => res.json(UserData))
+        .catch((err) => res.status(400).json(err));
+    },
+    // add friend
+    addFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $addToSet: { friends: params.friendId } },
+            { runValidators: true, new: true }
+        )
+        .then((UserData) => res.json(UserData))
+        .catch((err) => res.status(400).json(err));
+    },
+    // delete friend
+    deleteFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.userId },
+            { $pull: { friends: params.friendId } },
+            { runValidators: true, new: true }
+        )
+        .then((UserData) => res.json(UserData))
+        .catch((err) => res.status(400).json(err));
+    }
+};
+
